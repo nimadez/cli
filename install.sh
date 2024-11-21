8587fd7df2f29eb2240190147eb500e65039cd46
nimadez <111365948+nimadez@users.noreply.github.com>
2024-11-21 22:17:04 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,21 +6,21 @@ scr=$(realpath "$0")
 dir=$(dirname "$scr")
 
 for i in *.sh; do
-    sudo rm "/usr/local/bin/${i%%.sh}"
-    ln -s "$dir/$i" "/usr/local/bin/${i%%.sh}"
+    if [ "$i" != "install.sh" ]; then
+        sudo rm "/usr/local/bin/${i%%.sh}" 2>/dev/null
+        ln -s "$dir/$i" "/usr/local/bin/${i%%.sh}"
+    fi
 done
 
 for i in *.py; do
-    sudo rm "/usr/local/bin/${i%%.py}"
+    sudo rm "/usr/local/bin/${i%%.py}" 2>/dev/null
     ln -s "$dir/$i" "/usr/local/bin/${i%%.py}"
 done
 
 for i in *.js; do
-    sudo rm "/usr/local/bin/${i%%.js}"
+    sudo rm "/usr/local/bin/${i%%.js}" 2>/dev/null
     ln -s "$dir/$i" "/usr/local/bin/${i%%.js}"
 done
-
-sudo rm /usr/local/bin/install
 
 sudo chmod +x /usr/local/bin/*
 sudo chown -R $(whoami) /usr/local/bin
